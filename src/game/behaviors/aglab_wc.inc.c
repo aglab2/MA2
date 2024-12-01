80dbe7e9fbddf7dd01b5ae7a3e627a3126da8046
aglab2 <aglab3@gmail.com>
2024-12-01 12:02:01 +0800

    Added improvements for pushable head

--- src/game/behaviors/aglab_wc.inc.c
+++ src/game/behaviors/aglab_wc.inc.c
@@ -10,19 +10,23 @@ struct ObjectHitbox sWCStoneHeadBoxHitbox = {
     /* hurtboxHeight:     */ 400,
 };
 
+#define oWCStoneHeadAmountPushed oF4
+
 void bhv_wc_stonehead_loop(void) {
     obj_set_hitbox(o, &sWCStoneHeadBoxHitbox);
     o->oForwardVel = 0.0f;
 
+    if (o->oWCStoneHeadAmountPushed == 100)
+        return;
+
     if (obj_check_if_collided_with_object(o, gMarioObject) && gMarioStates[0].flags & MARIO_PUSHING) {
         s16 angleToMario = obj_angle_to_object(o, gMarioObject);
-        print_text_fmt_int(20, 20, "MA %d", (int) gMarioObject->oMoveAngleYaw);
-        print_text_fmt_int(20, 40, "OM %d", (int) angleToMario);
         if (abs_angle_diff(angleToMario, gMarioObject->oMoveAngleYaw) > 0x4000 && abs_angle_diff(0x8000, gMarioObject->oMoveAngleYaw) < 0x4000) {
             o->oMoveAngleYaw = (s16)((gMarioObject->oMoveAngleYaw + 0x2000) & 0xc000);
             if (check_if_moving_over_floor(8.0f, 150.0f)) {
                 o->oForwardVel = 4.0f;
                 cur_obj_play_sound_1(SOUND_ENV_METAL_BOX_PUSH);
+                o->oWCStoneHeadAmountPushed++;
             }
         }
     }
