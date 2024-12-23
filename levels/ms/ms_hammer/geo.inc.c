143c6f2a406da22b49a38f41bd33896f6443c7cc
aglab2 <aglab3@gmail.com>
2024-12-23 12:05:11 +0800

    Fixed hammer visuals

--- levels/ms/ms_hammer/geo.inc.c
+++ levels/ms/ms_hammer/geo.inc.c
@@ -3,7 +3,8 @@
 const GeoLayout ms_hammer_geo[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
-		GEO_DISPLAY_LIST(LAYER_OPAQUE, ms_hammer_ms_slane_mesh_layer_1),
+		GEO_DISPLAY_LIST(LAYER_OPAQUE, ms_hammer_ms_hammer_mesh_layer_1),
+		GEO_DISPLAY_LIST(LAYER_TRANSPARENT, ms_hammer_001_object_00D9724C_mesh_layer_5),
 	GEO_CLOSE_NODE(),
 	GEO_END(),
 };
--- levels/ms/script.c
+++ levels/ms/script.c
@@ -31,8 +31,17 @@ extern const GeoLayout ms_area_visual[];
 const LevelScript level_ms_entry[] = {
 	INIT_LEVEL(),
 	LOAD_MIO0(0x7, _ms_segment_7SegmentRomStart, _ms_segment_7SegmentRomEnd), 
+	LOAD_RAW(         /*seg*/ 0x0C, _group1_geoSegmentRomStart,  _group1_geoSegmentRomEnd),
+	LOAD_YAY0(        /*seg*/ 0x06, _group14_yay0SegmentRomStart, _group14_yay0SegmentRomEnd),
+	LOAD_RAW(         /*seg*/ 0x0D, _group14_geoSegmentRomStart,  _group14_geoSegmentRomEnd),
+	LOAD_YAY0(        /*seg*/ 0x08, _common0_yay0SegmentRomStart, _common0_yay0SegmentRomEnd),
+	LOAD_RAW(         /*seg*/ 0x0F, _common0_geoSegmentRomStart,  _common0_geoSegmentRomEnd),
 	ALLOC_LEVEL_POOL(),
-	MARIO(MODEL_MARIO, 0x00000001, bhvMario), 
+	MARIO(MODEL_MARIO, 0x00000001, bhvMario),
+	LOAD_MODEL_FROM_GEO(MODEL_MS_HAMMER, ms_hammer_geo),
+	JUMP_LINK(script_func_global_1),
+	JUMP_LINK(script_func_global_2),
+	JUMP_LINK(script_func_global_15),
 	/* Fast64 begin persistent block [level commands] */
 	/* Fast64 end persistent block [level commands] */
 
