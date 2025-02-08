14da6fcbf61b5d55271d3da5cb09029148564d59
aglab2 <aglab3@gmail.com>
2025-02-08 10:27:54 +0800

    Use nprim for some models

--- levels/aq/aq_milift/geo.inc.c
+++ levels/aq/aq_milift/geo.inc.c
@@ -1,7 +1,7 @@
 #include "src/game/envfx_snow.h"
 
 const GeoLayout aq_milift_geo[] = {
-	GEO_NODE_START(),
+	GEO_CULLING_RADIUS(30000),
 	GEO_OPEN_NODE(),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, aq_milift_aq_milift_mesh_layer_1),
 	GEO_CLOSE_NODE(),
