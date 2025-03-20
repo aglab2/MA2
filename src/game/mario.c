2c0994522ed0b2afdc6b6174106f9f02d729e7c5
aglab2 <aglab3@gmail.com>
2025-03-20 20:25:44 +0800

    Fixed floor alignment

--- src/game/mario.c
+++ src/game/mario.c
@@ -633,7 +633,7 @@ s16 find_floor_slope(struct MarioState *m, s16 yawOffset) {
     f32 x = sins(m->faceAngle[1] + yawOffset) * 5.0f;
     f32 z = coss(m->faceAngle[1] + yawOffset) * 5.0f;
 #ifdef FAST_FLOOR_ALIGN
-    if (absf(m->forwardVel) > FAST_FLOOR_ALIGN) {
+    if (!gGravityMode && absf(m->forwardVel) > FAST_FLOOR_ALIGN) {
         forwardFloorY  = get_surface_height_at_location(m->pos[0] + x, m->pos[2] + z, floor);
         backwardFloorY = get_surface_height_at_location(m->pos[0] - x, m->pos[2] - z, floor);
     } else {
