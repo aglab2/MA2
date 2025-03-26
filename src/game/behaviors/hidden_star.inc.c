b4d4b1a6ef1189120a4b2d79f1aa63f4ceee3c38
aglab2 <aglab3@gmail.com>
2025-03-26 21:18:02 +0800

    Added PC improvements, mark secrets

--- src/game/behaviors/hidden_star.inc.c
+++ src/game/behaviors/hidden_star.inc.c
@@ -30,6 +30,9 @@ void bhv_hidden_star_loop(void) {
 }
 
 void bhv_hidden_star_trigger_loop(void) {
+    if (0 == (o->oTimer % 16))
+        bhv_golden_coin_sparkles_loop();
+
     if (obj_check_if_collided_with_object(o, gMarioObject)) {
         struct Object *hiddenStar = cur_obj_nearest_object_with_behavior(bhvHiddenStar);
 
