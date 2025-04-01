b84ab806219b51ce0b16b2c9886e33d4761fea5d
aglab2 <aglab3@gmail.com>
2025-04-01 20:12:16 +0800

    Fixed crash when grabbing a star when having held object

--- src/game/interaction.c
+++ src/game/interaction.c
@@ -816,7 +816,9 @@ u32 interact_star_or_key(struct MarioState *m, UNUSED u32 interactType, struct O
     u32 grandStar = (obj->oInteractionSubtype & INT_SUBTYPE_GRAND_STAR) != 0;
 
     if (m->health >= 0x100) {
-        mario_stop_riding_and_holding(m);
+        if (IS_STAR_GRAB_EXIT(style))
+            mario_stop_riding_and_holding(m);
+
 #if ENABLE_RUMBLE
         queue_rumble_data(5, 80);
 #endif
