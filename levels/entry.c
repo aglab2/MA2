5251e2a1d8b4d1a0f6b3968593d45f8841e5ebd4
aglab2 <aglab3@gmail.com>
2025-06-15 23:07:41 +0800

    Added some work on CC switch

--- levels/entry.c
+++ levels/entry.c
@@ -9,7 +9,7 @@
 
 #include "config.h"
 
-#define TEST_LEVEL LEVEL_CCT
+#define TEST_LEVEL LEVEL_SH
 
 extern const LevelScript level_main_scripts_entry[];
 const LevelScript level_script_entry[] = {
