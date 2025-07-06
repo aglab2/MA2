113759500fd29222cc55e804e4e25a4fad9b7197
aglab2 <aglab3@gmail.com>
2025-07-06 18:16:48 +0800

    Fixed CCR jumps being too high

--- levels/ccr/area_1/spline.inc.c
+++ levels/ccr/area_1/spline.inc.c
@@ -8,14 +8,14 @@ const Trajectory ccr_area_1_spline_Spring_NurbsCurve[] = {
 const Trajectory ccr_area_1_spline_Spring_NurbsCurve_001[] = {
 	TRAJECTORY_POS( 0, 781, -1700, -6127),
 	TRAJECTORY_POS( 1, 749, -1348, -6094),
-	TRAJECTORY_POS( 2, 605, -978, -5950),
-	TRAJECTORY_POS( 3, 362, -886, -5736),
+	TRAJECTORY_POS( 2, 605, -1088, -5950),
+	TRAJECTORY_POS( 3, 362, -1263, -5736),
 	TRAJECTORY_END(),
 };
 const Trajectory ccr_area_1_spline_Spring_NurbsCurve_002[] = {
 	TRAJECTORY_POS( 0, -641, -1700, -6127),
 	TRAJECTORY_POS( 1, -609, -1348, -6094),
-	TRAJECTORY_POS( 2, -465, -978, -5950),
-	TRAJECTORY_POS( 3, -222, -886, -5736),
+	TRAJECTORY_POS( 2, -465, -1157, -5950),
+	TRAJECTORY_POS( 3, -222, -1253, -5736),
 	TRAJECTORY_END(),
 };
