b5e5c0920fa88a3c4a3bc861f0f813bc860fe4f8
aglab2 <aglab3@gmail.com>
2025-08-27 08:41:54 +0800

    Resample broken instruments

--- scripts/sound_check_samples.py
+++ scripts/sound_check_samples.py
@@ -1,14 +1,15 @@
 import os
 import soundfile as sf
 
-directory = '/run/media/admin/SSD/git/MA2/sound/samples/instruments' 
+directory = '../sound/samples/instruments' 
 
 for filename in os.listdir(directory):
     if filename.lower().endswith(('.aiff', '.aif')):
         filepath = os.path.join(directory, filename)
         try:
             with sf.SoundFile(filepath) as f:
-                print(f"{filename}: {f.samplerate} Hz")
+                if f.samplerate > 32000:
+                    print(f"WARNING {filename}: {f.samplerate} Hz")
         except Exception as e:
             print(f"Error reading {filename}: {e}")
 
