5ad0cce90c14573ae050d61a1430ac5f1fbfd534
aglab2 <aglab3@gmail.com>
2025-08-31 21:04:49 +0800

    Fixed WJ broken collision

--- levels/wj/model.inc.c
+++ levels/wj/model.inc.c
@@ -1368,6 +1368,13 @@ Vtx wj_dl_object_00A96A50_003_mesh_layer_1_vtx_1[48] = {
 	{{ {150, 213, 150}, 0, {2040, 2032}, {178, 178, 178, 255} }},
 };
 
+Vtx wj_dl_object_00A96E90_024_mesh_layer_1_vtx_0[4] = {
+	{{ {828, 1261, 70}, 0, {4096, -16}, {153, 185, 179, 255} }},
+	{{ {-247, -392, 70}, 0, {-16, 2032}, {157, 155, 178, 255} }},
+	{{ {-247, 1261, 70}, 0, {-16, -16}, {140, 140, 150, 255} }},
+	{{ {828, -392, 70}, 0, {4096, 2032}, {160, 160, 190, 255} }},
+};
+
 Vtx wj_dl_a3_003_mesh_layer_5_vtx_0[24] = {
 	{{ {-20000, 7012, 20000}, 0, {-16, 1008}, {0, 129, 0, 255} }},
 	{{ {-20000, 7012, -20000}, 0, {-16, -16}, {0, 129, 0, 255} }},
@@ -1692,6 +1699,20 @@ Gfx wj_dl_object_00A96A50_003_mesh_layer_1[] = {
 	gsSP2Triangles(36, 37, 38, 0, 37, 36, 39, 0),
 	gsSP2Triangles(40, 41, 42, 0, 41, 40, 43, 0),
 	gsSP2Triangles(44, 45, 46, 0, 45, 44, 47, 0),
+	gsDPPipeSync(),
+	gsSPLoadGeometryMode(G_ZBUFFER | G_SHADE | G_CULL_BACK | G_LIGHTING | G_SHADING_SMOOTH),
+	gsSPSetOtherMode(G_SETOTHERMODE_H, 4, 20, G_CD_MAGICSQ | G_AD_DISABLE | G_CK_NONE | G_TC_FILT | G_TF_BILERP | G_TT_NONE | G_TL_TILE | G_TD_CLAMP | G_TP_PERSP | G_CYC_1CYCLE | G_PM_1PRIMITIVE),
+	gsSPSetOtherMode(G_SETOTHERMODE_L, 0, 32, G_AC_NONE | G_ZS_PIXEL | G_RM_AA_ZB_OPA_SURF | G_RM_AA_ZB_OPA_SURF2),
+	gsSPEndDisplayList(),
+};
+
+Gfx wj_dl_object_00A96E90_024_mesh_layer_1[] = {
+	gsSPLoadGeometryMode(G_ZBUFFER | G_SHADE | G_CULL_BACK | G_FOG | G_SHADING_SMOOTH),
+	gsSPSetOtherMode(G_SETOTHERMODE_H, 4, 20, G_AD_NOISE | G_CD_MAGICSQ | G_CK_NONE | G_TC_FILT | G_TF_BILERP | G_TT_RGBA16 | G_TL_TILE | G_TD_CLAMP | G_TP_PERSP | G_CYC_2CYCLE | G_PM_1PRIMITIVE),
+	gsSPSetOtherMode(G_SETOTHERMODE_L, 0, 32, G_AC_NONE | G_ZS_PIXEL | G_RM_FOG_SHADE_A | G_RM_AA_ZB_OPA_SURF2),
+	gsDPSetTile(G_IM_FMT_CI, G_IM_SIZ_4b, 4, 0, 0, 0, G_TX_CLAMP | G_TX_NOMIRROR, 6, 0, G_TX_WRAP | G_TX_NOMIRROR, 6, 0),
+	gsSPVertex(wj_dl_object_00A96E90_024_mesh_layer_1_vtx_0 + 0, 4, 0),
+	gsSP2Triangles(0, 1, 2, 0, 1, 0, 3, 0),
 	gsDPPipeSync(),
 	gsSPLoadGeometryMode(G_ZBUFFER | G_SHADE | G_CULL_BACK | G_LIGHTING | G_SHADING_SMOOTH),
 	gsSPSetOtherMode(G_SETOTHERMODE_H, 4, 20, G_CD_MAGICSQ | G_AD_DISABLE | G_CK_NONE | G_TC_FILT | G_TF_BILERP | G_TT_NONE | G_TL_TILE | G_TD_CLAMP | G_TP_PERSP | G_CYC_1CYCLE | G_PM_1PRIMITIVE),
