a298b99653f147924e370179d52b6aa4c82f3a42
aglab2 <aglab3@gmail.com>
2025-09-04 21:16:20 +0800

    Fixed SR rails and springs

--- levels/sr/rails.inc.c
+++ levels/sr/rails.inc.c
@@ -21,9 +21,16 @@ static const RailDesc rails_area1[] = {
 	RAIL_END
 };
 
+static const SpringDesc spring_area2[] = {
+	sr_area_2_spline_NurbsCurve_Spring_004,
+	NULL
+};
+
 static const RailDesc rails_area2[] = {
 	RAIL_TRAJ(sr_area_2_spline_0061_object_00AFC50C_001),
 	RAIL_TRAJ(sr_area_2_spline_0065_object_00AF8344_002),
+	RAIL_TRAJ(sr_area_2_spline_0078_object_00AF103C_001_Rail),
+	RAIL_TRAJ(sr_area_2_spline_0078_object_00AF103C_001_Rail_001),
 	RAIL_TRAJ(sr_area_2_spline_0080_object_00AF037C_002_Rail),
 	RAIL_TRAJ(sr_area_2_spline_0107_object_00ADF904_001_Rail),
 	RAIL_TRAJ(sr_area_2_spline_NurbsCurve_001_Rail_001),
@@ -47,7 +54,7 @@ static const RailDesc rails_area3[] = {
 
 const SpringDesc* spring_descs_sr[] = {
 	spring_area1,
-	NULL,
+	spring_area2,
 	spring_area3,
 };
 
