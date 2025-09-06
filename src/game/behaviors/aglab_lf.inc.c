dd35b2c12e80e13334d349c418bf2cf5ff017037
aglab2 <aglab3@gmail.com>
2025-09-06 21:44:20 +0800

    Added bowser hitting drop implementation in LF

--- src/game/behaviors/aglab_lf.inc.c
+++ src/game/behaviors/aglab_lf.inc.c
@@ -9,7 +9,6 @@ void bhv_lf_ctl_init()
 
 void bhv_lf_ctl_loop()
 {
-    o->parentObj->oFaceAngleYaw = 0;
     o->parentObj->oPosX = 0;
     o->parentObj->oPosY = -500;
     o->parentObj->oPosZ = 0;
@@ -20,5 +19,34 @@ void bhv_lf_ctl_loop()
      && o->parentObj->oAction != BOWSER_ACT_DEAD)
     {
         o->parentObj->oAction = 0;
+    }
+
+    gMarioStates->pos[0] = CLAMP(gMarioStates->pos[0], -3000.f, 3000.f);
+
+    int sideSplit = o->parentObj->oHealth & 1;
+    if (0 == o->oAction)
+    {
+        o->parentObj->oFaceAngleYaw = sideSplit ? 0x8000 : 0;
+        int act = o->parentObj->oAction;
+        if (act == BOWSER_ACT_HIT_MINE)
+        {
+            o->oAction = 1;
+            int initAngle = sideSplit ? 0x8000 : 0;
+            s8DirModeYawOffset = initAngle;
+            gMarioStates->faceAngle[1] = 0x8000 - initAngle;
+        }
+    }
+    else
+    {
+        int side = sideSplit ? -1 : 1;
+        int angle = CLAMP(o->oTimer * 0x200, 0, 0x8000);
+        o->parentObj->oFaceAngleYaw = sideSplit ? angle : (0x8000 - angle);
+        gMarioStates->pos[0] = 0;
+        gMarioStates->pos[2] = side * 160 * o->oTimer;
+
+        if (o->oTimer == 105)
+        {
+            o->oAction = 0;
+        }
     }
 }
