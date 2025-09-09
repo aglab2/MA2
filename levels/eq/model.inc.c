1c73e5081eeb144c44f3cc71a0533dc0bfc59bc5
aglab2 <aglab3@gmail.com>
2025-09-09 21:19:53 +0800

    Fixed GBJ in EQ

--- levels/eq/model.inc.c
+++ levels/eq/model.inc.c
@@ -42475,7 +42475,7 @@ Gfx mat_eq_dl_Water1_sa2mdl_0_f3d[] = {
 	gsDPSetAlphaDither(G_AD_NOISE),
 	gsDPSetTextureLUT(G_TT_RGBA16),
 	gsSPTexture(65535, 65535, 0, 0, 1),
-	gsDPSetPrimColor(0, 0, 255, 255, 255, 151),
+	gsDPSetPrimColor(0, 0, 255, 255, 255, 255),
 	gsDPSetTextureImage(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, eq_dl_kd_eq_water01_pal_rgba16),
 	gsDPSetTile(0, 0, 0, 256, 5, 0, G_TX_WRAP | G_TX_NOMIRROR, 0, 0, G_TX_WRAP | G_TX_NOMIRROR, 0, 0),
 	gsDPLoadTLUTCmd(5, 15),
