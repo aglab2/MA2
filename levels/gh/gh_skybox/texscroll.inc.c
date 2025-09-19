ef2b529ba4270f0b7a89f3ff94f0ea7f9bc0310a
aglab2 <aglab3@gmail.com>
2025-09-19 09:05:19 +0800

    Added GH skybox

--- levels/gh/gh_skybox/texscroll.inc.c
+++ levels/gh/gh_skybox/texscroll.inc.c
@@ -2,8 +2,8 @@ void scroll_gfx_gh_skybox_gh_skybox_mesh() {
 	Gfx *mat = segmented_to_virtual(gh_skybox_gh_skybox_mesh);
 
 
-	shift_t(mat, 305, PACK_TILESIZE(0, 2));
-	shift_t(mat, 344, PACK_TILESIZE(0, 2));
+	shift_t(mat, 360, PACK_TILESIZE(0, 2));
+	shift_t(mat, 399, PACK_TILESIZE(0, 2));
 
 };
 
