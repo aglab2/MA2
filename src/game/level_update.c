68a11b98b88fcdf5e03e558474294dd62c0f5bc9
aglab2 <aglab3@gmail.com>
2025-09-20 10:52:32 +0800

    Reduce delayed warp timer a bit

--- src/game/level_update.c
+++ src/game/level_update.c
@@ -912,7 +912,7 @@ s16 level_trigger_warp(struct MarioState *m, s32 warpOp) {
                 break;
 
             case WARP_OP_SPIN_SHRINK: // bbh enter
-                sDelayedWarpTimer = 100;
+                sDelayedWarpTimer = 80;
                 sSourceWarpNodeId = GET_BPARAM2(m->usedObj->oBehParams);
                 play_transition(WARP_TRANSITION_FADE_INTO_COLOR, 60, 0, 0, 0);
                 break;
