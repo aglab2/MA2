eb9aca8221b45ee63acea65f7079b28fc4faf71c
aglab2 <aglab3@gmail.com>
2025-09-22 22:44:59 +0800

    Fixed shutter in CCS

--- levels/ccs/ccs_shutter_left/geo.inc.c
+++ levels/ccs/ccs_shutter_left/geo.inc.c
@@ -1,8 +1,10 @@
 #include "src/game/envfx_snow.h"
 
+extern Gfx *geo_update_transparency(s32 callContext, struct GraphNode *node, UNUSED void *context);
 const GeoLayout ccs_shutter_left_geo[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
+		GEO_ASM(GEO_TRANSPARENCY_MODE_NORMAL, geo_update_transparency),
 		GEO_TRANSLATE_NODE_WITH_DL(LAYER_TRANSPARENT, (s16)-185.0, (s16)300.0, (s16)119.99999046325684, ccs_shutter_left_001_object_012432C4_mesh_layer_5),
 		GEO_TRANSLATE_NODE_WITH_DL(LAYER_OPAQUE, (s16)-185.0, (s16)300.0, (s16)119.99999046325684, ccs_shutter_left_002_object_0124316C_mesh_layer_1),
 		GEO_TRANSLATE_NODE_WITH_DL(LAYER_TRANSPARENT, (s16)-185.0, (s16)300.0, (s16)119.99999046325684, ccs_shutter_left_004_object_01242574_mesh_layer_5),
