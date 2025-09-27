e49a684c94a9d150f5c4199ed48ff233746d9eb0
aglab2 <aglab3@gmail.com>
2025-09-27 20:06:54 +0800

    Added SR anims, fixed ending collision

--- src/game/behaviors/aglab_geocmn.inc.c
+++ src/game/behaviors/aglab_geocmn.inc.c
@@ -19,3 +19,25 @@ Gfx *geo_lvlbatch_rotate2(s32 callContext, struct GraphNode *node, UNUSED s32 co
     }
     return NULL;
 }
+
+Gfx *geo_lvlbatch_rotate3(s32 callContext, struct GraphNode *node, UNUSED s32 context)
+{
+    if (callContext == GEO_CONTEXT_RENDER) {
+        struct GraphNodeGenerated *fnNode = (struct GraphNodeGenerated *) node;
+        int param = fnNode->parameter;
+        struct GraphNodeBatchsetTranslationRotation *transNode = (struct GraphNodeBatchsetTranslationRotation *) node->next;
+        transNode->rotation[0] += param;
+    }
+    return NULL;
+}
+
+Gfx *geo_lvlbatch_rotate4(s32 callContext, struct GraphNode *node, UNUSED s32 context)
+{
+    if (callContext == GEO_CONTEXT_RENDER) {
+        struct GraphNodeGenerated *fnNode = (struct GraphNodeGenerated *) node;
+        int param = fnNode->parameter;
+        struct GraphNodeBatchsetTranslationRotation *transNode = (struct GraphNodeBatchsetTranslationRotation *) node->next;
+        transNode->rotation[2] += param;
+    }
+    return NULL;
+}
