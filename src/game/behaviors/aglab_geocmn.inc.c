8f50c32ccb2ed9eb941ee0b5bb0a156cd380cbfb
aglab2 <aglab3@gmail.com>
2025-09-27 18:40:32 +0800

    Animate RH

--- src/game/behaviors/aglab_geocmn.inc.c
+++ src/game/behaviors/aglab_geocmn.inc.c
@@ -8,3 +8,14 @@ Gfx *geo_lvlbatch_rotate(s32 callContext, struct GraphNode *node, UNUSED s32 con
     }
     return NULL;
 }
+
+Gfx *geo_lvlbatch_rotate2(s32 callContext, struct GraphNode *node, UNUSED s32 context)
+{
+    if (callContext == GEO_CONTEXT_RENDER) {
+        struct GraphNodeGenerated *fnNode = (struct GraphNodeGenerated *) node;
+        int param = fnNode->parameter;
+        struct LightGraphLvlNodeTranslationRotation *transNode = (struct LightGraphLvlNodeTranslationRotation *) node->next;
+        transNode->rotation[1] += param;
+    }
+    return NULL;
+}
