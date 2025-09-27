b262e22ace65308635e771de92028bb922c5d6bc
aglab2 <aglab3@gmail.com>
2025-09-27 17:38:32 +0800

    Remove more useless code

--- src/game/behavior_data.c
+++ src/game/behavior_data.c
@@ -358,7 +358,7 @@ const BehaviorScript bhvCannon[] = {
     SET_HITBOX(/*Radius*/ 150, /*Height*/ 150),
     SET_INT(oIntangibleTimer, 0),
     BEGIN_LOOP(),
-        CALL_NATIVE(bhv_cannon_base_loop),
+        // CALL_NATIVE(bhv_cannon_base_loop),
     END_LOOP(),
 };
 
@@ -367,7 +367,7 @@ const BehaviorScript bhvCannonBarrel[] = {
     OR_INT(oFlags, (OBJ_FLAG_ACTIVE_FROM_AFAR | OBJ_FLAG_COMPUTE_DIST_TO_MARIO | OBJ_FLAG_SET_FACE_YAW_TO_MOVE_YAW | OBJ_FLAG_UPDATE_GFX_POS_AND_ANGLE)),
     DROP_TO_FLOOR(),
     BEGIN_LOOP(),
-        CALL_NATIVE(bhv_cannon_barrel_loop),
+        // CALL_NATIVE(bhv_cannon_barrel_loop),
     END_LOOP(),
 };
 
@@ -2190,17 +2190,7 @@ const BehaviorScript bhvLllBowserPuzzle[] = {
 
 const BehaviorScript bhvTuxiesMother[] = {
     BEGIN(OBJ_LIST_GENACTOR),
-    OR_INT(oFlags, (OBJ_FLAG_COMPUTE_ANGLE_TO_MARIO | OBJ_FLAG_COMPUTE_DIST_TO_MARIO | OBJ_FLAG_SET_FACE_YAW_TO_MOVE_YAW | OBJ_FLAG_UPDATE_GFX_POS_AND_ANGLE)),
-    LOAD_ANIMATIONS(oAnimations, penguin_seg5_anims_05008B74),
-    ANIMATE(PENGUIN_ANIM_IDLE),
-    SET_OBJ_PHYSICS(/*Wall hitbox radius*/ 30, /*Gravity*/ -400, /*Bounciness*/ -50, /*Drag strength*/ 0, /*Friction*/ 0, /*Buoyancy*/ 0, /*Unused*/ 0, 0),
-    SET_HOME(),
-    SET_INTERACT_TYPE(INTERACT_TEXT),
-    SET_HITBOX(/*Radius*/ 200, /*Height*/ 300),
-    SET_INT(oIntangibleTimer, 0),
-    BEGIN_LOOP(),
-        CALL_NATIVE(bhv_tuxies_mother_loop),
-    END_LOOP(),
+    BREAK(),
 };
 
 const BehaviorScript bhvPenguinBaby[] = {
