c6a47e254ec88ee584757e0876f9ed7aea7848dd
aglab2 <aglab3@gmail.com>
2025-09-30 09:54:17 +0800

    Fixed DC sushi

--- src/game/behaviors/sushi.inc.c
+++ src/game/behaviors/sushi.inc.c
@@ -1,20 +1,29 @@
 // sushi.inc.c
 
 void bhv_sushi_shark_loop(void) {
-    f32 waterLevel = find_water_level(o->oPosX, o->oPosZ);
+    o->oPosX = sins(o->oSushiSharkAngleFromHome) * 1700.0f;
+    o->oPosZ = coss(o->oSushiSharkAngleFromHome) * 1700.0f;
+    o->oPosY = o->oHomeY + sins(o->oSushiSharkAngleFromHome) * 200.0f;
 
-    o->oPosX = o->oHomeX + sins(o->oSushiSharkAngleFromHome) * 1700.0f;
-    o->oPosZ = o->oHomeZ + coss(o->oSushiSharkAngleFromHome) * 1700.0f;
-    o->oPosY = waterLevel + o->oHomeY + sins(o->oSushiSharkAngleFromHome) * 200.0f;
+    if (o->oHomeY > -2000.f)
+    {    
+        o->oSushiSharkAngleFromHome += 0x80;
+        o->oMoveAngleYaw = o->oSushiSharkAngleFromHome + 0x4000;
+    }
+    else
+    {
+        o->oSushiSharkAngleFromHome -= 0x93;
+        o->oMoveAngleYaw = o->oSushiSharkAngleFromHome - 0x4000;
+    }
 
-    o->oMoveAngleYaw = o->oSushiSharkAngleFromHome + 0x4000;
-    o->oSushiSharkAngleFromHome += 0x80;
-
+#if 0
     if (gMarioObject->oPosY - waterLevel > -500.0f) {
         if (o->oPosY - waterLevel > -200.0f) {
             spawn_object_with_scale(o, MODEL_WAVE_TRAIL, bhvObjectWaveTrail, 4.0f);
         }
     }
+#endif
+
     if (!(o->oTimer & 0x0F)) {
         cur_obj_play_sound_2(SOUND_OBJ_SUSHI_SHARK_WATER_SOUND);
     }
